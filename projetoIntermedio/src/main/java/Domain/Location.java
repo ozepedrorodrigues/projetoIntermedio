def37cae51fcbac3024c6124cfb96ce92200a23a
pedrosampaio <1231853@isep.ipp.pt>
2024-02-24 15:29:13 +0000

    location + testes

--- projetoIntermedio/src/main/java/Domain/Location.java
+++ projetoIntermedio/src/main/java/Domain/Location.java
@@ -7,31 +7,45 @@ import Factories.GPSLocationFactory;
  * The location includes the address, zip code, and GPS location.
  */
 public class Location {
+
     /**
      * the address of the house (Street, Number, Door)
      */
     private String address;
+
     /**
      * the zip code of the house
      */
     private String zipCode;
+
     /**
      * GPS location of the house
      */
     private GPSLocation gpsLocation;
 
     /**
+     * GPSLocationFactory of the location
+     */
+    private GPSLocationFactory gpsLocationFactory;
+
+    /**
      * Constructor
      *
-     * @param address the address of the house (Street, Number, Door)
-     * @param zipCode the zip code of the house
+     * @param address            the address of the house (Street, Number, Door)
+     * @param zipCode            the zip code of the house
+     * @param latitude           the latitude of the GPS location of the house
+     * @param longitude          the longitude of the GPS location of the house
+     * @param gpsLocationFactory the GPSLocationFactory of the location
      */
     public Location(String address, String zipCode, double latitude, double longitude, GPSLocationFactory gpsLocationFactory) {
-        if (address.isBlank() || zipCode.isBlank())
-            throw new IllegalArgumentException("Invalid Address or ZipCode");
+        if (!validAddress(address) || !validZipCode(zipCode) || !validGpsFactory(gpsLocationFactory)) {
+            throw new IllegalArgumentException("Invalid parameters");
+        }
         this.address = address;
         this.zipCode = zipCode;
-        this.gpsLocation = gpsLocationFactory.createGPSLocation(latitude,longitude);}
+        this.gpsLocationFactory = gpsLocationFactory;
+        this.gpsLocation = this.gpsLocationFactory.createGPSLocation(latitude, longitude);
+    }
 
     /**
      * Getter for the address of the house
@@ -58,5 +72,35 @@ public class Location {
      */
     public GPSLocation getGpsLocation() {
         return gpsLocation;
+    }
+
+    /**
+     * Getter for the GPSLocationFactory of the location
+     *
+     * @return the GPS factory of the location
+     */
+    public GPSLocationFactory getGpsLocationFactory() {
+        return gpsLocationFactory;
+    }
+
+    /**
+     * Checks if the given address is non-null and not blank.
+     */
+    private boolean validAddress(String address) {
+        return address != null && !address.isBlank();
+    }
+
+    /**
+     * Checks if the given zip code is non-null and not blank.
+     */
+    private boolean validZipCode(String zipCode) {
+        return zipCode != null && !zipCode.isBlank();
+    }
+
+    /**
+     * Checks if the given GPSLocationFactory is non-null.
+     */
+    private boolean validGpsFactory(GPSLocationFactory gpsLocationFactory) {
+        return gpsLocationFactory != null;
     }
 }
