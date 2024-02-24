95bbd7fa41cb0e43742c85baa453cd3d1c1de05d
ineslemos <ines__lemos@hotmail.com>
2024-02-24 11:10:21 +0000

    room factories

--- projetoIntermedio/src/main/java/Factories/implement/RoomFactoryImp.java
+++ projetoIntermedio/src/main/java/Factories/implement/RoomFactoryImp.java
@@ -3,15 +3,19 @@ package Factories.implement;
 import Domain.Room;
 import Factories.DeviceFactory;
 import Factories.DimensionsFactory;
+import Factories.RoomFactory;
 
-public class RoomFactoryImp {
+public class RoomFactoryImp implements RoomFactory {
     public RoomFactoryImp() {
     }
 
-    public Room createRoom(String name, int floor, double width, double length, double height, DimensionsFactory dimensionsFactory) {
+    public Room createRoom(String name, int floor, double width, double length, double height) {
         DeviceFactory deviceFactory = new DeviceFactoryImp();
-        try{return new Room(name, floor, width,length,height,dimensionsFactory);}
-        catch (IllegalArgumentException e) {
-            throw new IllegalArgumentException(e.getMessage());}
+        DimensionsFactory dimensionsFactory = new DimensionsFactoryImp();
+        try {
+            return new Room(name, floor, width, length, height, dimensionsFactory, deviceFactory);
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException(e.getMessage());
+        }
     }
 }
