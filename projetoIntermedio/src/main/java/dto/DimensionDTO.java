53c4dd6aa1658ba9352062022f2f59c79dd6d21e
Pedro <1231852@isep.ipp.pt>
2024-02-27 01:17:17 +0000

    add DimensionDTO

---
+++ projetoIntermedio/src/main/java/dto/DimensionDTO.java
@@ -0,0 +1,51 @@
+package dto;
+
+public class DimensionDTO {
+
+    /**
+     * The width of the room.
+     */
+    private double width;
+
+    /**
+     * The length of the room.
+     */
+    private double length;
+
+    /**
+     * The height of the room.
+     */
+    private double height;
+
+
+
+    /**
+     * Returns the width of the room.
+     *
+     * @return the width of the room
+     */
+    public double getWidth() {
+        return width;
+    }
+
+    /**
+     * Returns the length of the room.
+     *
+     * @return the length of the room
+     */
+    public double getLength() {
+        return length;
+    }
+
+    /**
+     * Returns the height of the room.
+     *
+     * @return the height of the room
+     */
+    public double getHeight() {
+        return height;
+    }
+
+
+
+}
