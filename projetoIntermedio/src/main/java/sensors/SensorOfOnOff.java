7096716cd330762c936bceab4a58ad8d34d02666
pedrosampaio <1231853@isep.ipp.pt>
2024-02-28 00:50:44 +0000

    javadocs e cenas

--- projetoIntermedio/src/main/java/sensors/SensorOfOnOff.java
+++ projetoIntermedio/src/main/java/sensors/SensorOfOnOff.java
@@ -4,13 +4,22 @@ import domain.SensorType;
 import factories.ValueFactory;
 import values.Value;
 
+/**
+ * Represents a sensor of type ON_OFF.
+ */
 public class SensorOfOnOff implements Sensor {
 
     private int id;
     private SensorType type;
     private Value value;
 
-    public SensorOfOnOff(ValueFactory valueFactory) {
+    /**
+     * Constructs a SensorOfOnOff instance using the provided ValueFactory.
+     *
+     * @param valueFactory The ValueFactory used to create the initial ON/OFF value for the sensor.
+     * @throws IllegalArgumentException If the provided ValueFactory is null.
+     */
+    public SensorOfOnOff(ValueFactory valueFactory) throws IllegalArgumentException {
         if (!validValueFactory(valueFactory)) {
             throw new IllegalArgumentException("Invalid parameters");
         }
@@ -18,27 +27,55 @@ public class SensorOfOnOff implements Sensor {
         this.value = valueFactory.createOnOffValue();
     }
 
+    /**
+     * Retrieves the type of the sensor, which is ON_OFF.
+     *
+     * @return The SensorType.ON_OFF enumeration representing the sensor type.
+     */
     @Override
     public SensorType getType() {
-        return null;
+        return this.type;
     }
 
+    /**
+     * Retrieves the current ON/OFF value of the sensor.
+     *
+     * @return The current ON/OFF value as a Value object.
+     */
     @Override
     public Value getValue() {
-        return null;
+        return this.value;
     }
 
+    /**
+     * Sets the ID of the sensor to the specified value.
+     *
+     * @param newId The new ID to set for the sensor.
+     * @return The updated ID after the operation.
+     */
     @Override
     public int setId(int newId) {
-        return 0;
+        this.id = newId;
+        return this.id;
     }
 
+    /**
+     * Retrieves the ID of the sensor.
+     *
+     * @return The current ID of the sensor.
+     */
     @Override
     public int getId() {
-        return 0;
+        return this.id;
     }
 
-    private boolean validValueFactory (ValueFactory valueFactory){
+    /**
+     * Checks if the provided ValueFactory is not null.
+     *
+     * @param valueFactory The ValueFactory to validate.
+     * @return true if the ValueFactory is not null, false otherwise.
+     */
+    private boolean validValueFactory(ValueFactory valueFactory) {
         return valueFactory != null;
     }
 }
--- projetoIntermedio/src/main/java/values/OnOffValue.java
+++ projetoIntermedio/src/main/java/values/OnOffValue.java
@@ -1,20 +1,29 @@
 package values;
 
-
+/**
+ * Represents an ON/OFF value, providing methods to retrieve the boolean ON/OFF state
+ * and convert the value to a string representation.
+ */
 public class OnOffValue implements Value {
 
     private boolean onOffValue;
 
-    public OnOffValue() {
-    }
-
+    /**
+     * Retrieves the boolean ON/OFF state of the value.
+     *
+     * @return true if the value represents ON, false if it represents OFF.
+     */
     public boolean getOnOffValue() {
         return onOffValue;
     }
 
+    /**
+     * Converts the ON/OFF value to its string representation.
+     *
+     * @return A string representation of the ON/OFF value.
+     */
     @Override
     public String valueToString() {
         return String.valueOf(onOffValue);
     }
-
 }
