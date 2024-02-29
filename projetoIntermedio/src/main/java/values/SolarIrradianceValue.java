2433ca2a195d410c5a89b363deb0a5a46230fbe9
PedroSiqueira <1231855@isep.ipp.pt>
2024-02-29 14:11:18 +0000

    Criação e Javadoc SolarIrradianceValue

---
+++ projetoIntermedio/src/main/java/values/SolarIrradianceValue.java
@@ -0,0 +1,37 @@
+package values;
+
+/**
+ * This class represents a solar irradiance value.
+ */
+public class SolarIrradianceValue implements Value {
+
+    /**
+     * The solar irradiance value.
+     */
+    private double solarIrradianceValue;
+
+    /**
+     * Constructor for the SolarIrradianceValue class.
+     * @param solarIrradianceValue The solar irradiance value.
+     */
+    public SolarIrradianceValue(double solarIrradianceValue) {
+        this.solarIrradianceValue = solarIrradianceValue;
+    }
+
+    /**
+     * @return the solar irradiance value.
+     */
+    public double getSolarIrradianceValue() {
+        return solarIrradianceValue;
+    }
+
+    /**
+     * @return the solar irradiance value as a string.
+     */
+    @Override
+    public String valueToString() {
+        return String.valueOf(solarIrradianceValue);
+    }
+
+}
+
