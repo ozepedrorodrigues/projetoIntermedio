fcbddde244790c4d632438bc0acc12aa9225e6b9
PedroSiqueira <1231855@isep.ipp.pt>
2024-02-29 14:02:57 +0000

    Javadoc SensorOfSolarIrradiance

--- projetoIntermedio/src/main/java/sensors/SensorOfSolarIrradiance.java
+++ projetoIntermedio/src/main/java/sensors/SensorOfSolarIrradiance.java
@@ -4,6 +4,9 @@ import domain.SensorType;
 import factories.ValueFactory;
 import values.Value;
 
+/**
+ * This class represents a sensor of solar irradiance.
+ */
 public class SensorOfSolarIrradiance {
 
     private int id;
@@ -14,7 +17,11 @@ public class SensorOfSolarIrradiance {
 
     private ValueFactory valueFactory;
 
-
+    /**
+     * Constructor for the SensorOfSolarIrradiance class.
+     * @param valueFactory The ValueFactory instance used to create values for this sensor.
+     * @throws IllegalArgumentException If the provided valueFactory is null.
+     */
     public SensorOfSolarIrradiance(ValueFactory valueFactory) {
         if (!validValueFactory(valueFactory)) {
             throw new IllegalArgumentException("Invalid parameters");
@@ -25,22 +32,42 @@ public class SensorOfSolarIrradiance {
         }
     }
 
+    /**
+     * Checks if the provided ValueFactory instance is valid.
+     * @param valueFactory The ValueFactory instance to check.
+     * @return true if the provided ValueFactory instance is not null, false otherwise.
+     */
     private boolean validValueFactory(ValueFactory valueFactory) {
         return valueFactory != null;
     }
 
+
+    /**
+     * @return the ID of the sensor.
+     */
     public int getId() {
         return id;
     }
 
-    public int setId (int newId) {
+    /**
+     * Sets the ID of the sensor.
+     * @param newId The new ID to set.
+     * @return the updated ID of the sensor.
+     */
+    public int setId(int newId) {
         return id = newId;
     }
 
+    /**
+     * @return the type of the sensor.
+     */
     public SensorType getType() {
         return type;
     }
 
+    /**
+     * @return the value of the sensor.
+     */
     public Value getValue() {
         return value;
     }
