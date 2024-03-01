a0998603f720920f932fc1c1328a4b915c754b63
miguelapcouto94 <1231847@isep.ipp.pt>
2024-03-01 10:55:49 +0000

    update SensorOfWindSpeed

--- projetoIntermedio/src/main/java/sensors/SensorOfWindSpeed.java
+++ projetoIntermedio/src/main/java/sensors/SensorOfWindSpeed.java
@@ -1,35 +1,79 @@
 package sensors;
 
 import domain.SensorType;
-import factories.ValueFactory;
 import values.Value;
 import values.WindSpeedValue;
 
+/**
+ * This class represents a sensor of wind speed.
+ * It implements the Sensor interface.
+ * The sensor has an ID, a type, and a value.
+ * The type is always SensorType.WIND_SPEED, and the value is a WindSpeedValue.
+ * The ID can be set using the setID method.
+ */
 public class SensorOfWindSpeed implements Sensor {
 
-    private SensorType type;
+    /**
+     * The ID of the sensor.
+     */
     private int id;
+
+    /**
+     * The type of the sensor.
+     */
+    private SensorType type;
+
+    /**
+     * The default value of the sensor.
+     */
     private Value DEFAULT = new WindSpeedValue(25.0);
 
-    public SensorOfWindSpeed(ValueFactory valueFactory) {
+    /**
+     * Constructor for the SensorOfWindSpeed class.
+     */
+
+    public SensorOfWindSpeed() {
         this.type = SensorType.WIND_SPEED;
     }
+
+    /**
+     * Returns the ID of the sensor.
+     *
+     * @return the ID of the sensor
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * Sets the ID of the sensor.
+     *
+     * @param newId the new ID of the sensor
+     * @return the ID of the sensor
+     */
+    public int setId(int newId) {
+        this.id = newId;
+        return id;
+    }
+
+    /**
+     * Returns the type of the sensor.
+     *
+     * @return the type of the sensor
+     */
 
     public SensorType getType() {
         return type;
     }
 
-    public int getId() {
-        return id;
-    }
-
-    public int setId(int newId) {
-        return id = newId;
-    }
-
+    /**
+     * Returns the value of the sensor.
+     *
+     * @return
+     */
     public Value getValue() {
         ;
-        return this.DEFAULT;
+        return this.DEFAULT;// for now is a default value.
     }
 
 }
---
+++ projetoIntermedio/src/test/java/values/WindSpeedValueTest.java
@@ -0,0 +1,54 @@
+package values;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * This class contains unit tests for the WindSpeedValue class.
+ */
+
+class WindSpeedValueTest {
+
+    /**
+     * The WindSpeedValue object to be tested.
+     */
+    WindSpeedValue windSpeedValue;
+
+    /**
+     * Initializes the windSpeedValue object for the tests.
+     */
+    @BeforeEach
+    void setUp() {
+        windSpeedValue = new WindSpeedValue(25);
+    }
+
+    /**
+     * Verifies the WindSpeedValue constructor sets the value correctly.
+     * Verifies the getValue method returns the expected wind speed value.
+     */
+    @Test
+    void defaultConstructor_getWindSpeedValue() {
+        // Arrange
+        double expected = 25;
+        // Act
+        double result = windSpeedValue.getValue();
+        // Assert
+        assertEquals(expected, result, 0.01);
+    }
+
+    /**
+     * Verifies that the toValue method generates the correct string representation.
+     */
+    @Test
+    void toValue_Valid() {
+        // Arrange
+        String expected = "25.0";
+        // Act
+        String result = windSpeedValue.valueToString();
+        // Assert
+        assertEquals(expected, result);
+    }
+
+}
