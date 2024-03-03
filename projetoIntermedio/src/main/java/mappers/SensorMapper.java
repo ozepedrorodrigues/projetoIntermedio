2268d864a9ab550b6435f8ffd83e2cfeb8a47c06
PedroSiqueira <1231855@isep.ipp.pt>
2024-03-03 17:16:29 +0000

    Correcao SensorMapper

--- projetoIntermedio/src/main/java/mappers/SensorMapper.java
+++ projetoIntermedio/src/main/java/mappers/SensorMapper.java
@@ -20,6 +20,7 @@ public class SensorMapper {
     /**
      * This method maps a Sensor object to a SensorDTO object.
      * It retrieves the id and type of the sensor and uses them to create a new SensorDTO object.
+     *
      * @param sensor a Sensor object to be mapped to a SensorDTO object.
      * @return a SensorDTO object that contains the id and type of the sensor.
      */
