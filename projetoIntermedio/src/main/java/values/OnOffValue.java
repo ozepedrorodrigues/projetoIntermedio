e12ea7c8916d30e5849dd9cdf649865f8f18164e
pedrosampaio <1231853@isep.ipp.pt>
2024-03-04 19:17:42 +0000

    sensor on off

--- projetoIntermedio/src/main/java/values/OnOffValue.java
+++ projetoIntermedio/src/main/java/values/OnOffValue.java
@@ -1,24 +1,28 @@
 package values;
 
 /**
- * Represents an ON/OFF value, providing methods to retrieve the boolean ON/OFF state
- * and convert the value to a string representation.
+ * This class represents a binary value that can be either ON or OFF.
  */
 public class OnOffValue implements Value {
 
+    /**
+     * The ON/OFF value.
+     */
     private boolean onOffValue;
 
     /**
-     * Constructor for the OnOffValue class.
+     * Constructs a new instance of the OnOffValue class with the ON/OFF value initialized to an undefined state.
+     *
+     * @param onOffValue The ON/OFF value.
      */
     public OnOffValue(boolean onOffValue) {
         this.onOffValue = onOffValue;
     }
 
     /**
-     * Converts the ON/OFF value to its string representation.
+     * Converts the ON/OFF value to a string representation.
      *
-     * @return A string representation of the ON/OFF value.
+     * @return The string representation of the ON/OFF value.
      */
     @Override
     public String valueToString() {
