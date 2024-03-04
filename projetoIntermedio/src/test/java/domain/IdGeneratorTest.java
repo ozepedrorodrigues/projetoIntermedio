420f81d17d52b4171b8d322c2a4a1e77154e10e5
miguelapcouto94 <1231847@isep.ipp.pt>
2024-03-04 10:33:11 +0000

    update IdGeneratorTest, Actuatorid Tests

--- projetoIntermedio/src/test/java/domain/IdGeneratorTest.java
+++ projetoIntermedio/src/test/java/domain/IdGeneratorTest.java
@@ -4,12 +4,30 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * Test class for the IdGenerator class.
+ */
 class IdGeneratorTest {
 
+    /**
+     * Test of generateActuatorId method.
+     * The method should return a unique id for each call.
+     */
     @Test
     void generateSensorId() {
         assertEquals(1, IdGenerator.generateSensorId());
         assertEquals(2, IdGenerator.generateSensorId());
         assertEquals(3, IdGenerator.generateSensorId());
+    }
+
+    /**
+     * Test of generateActuatorId method.
+     * The method should return a unique id for each call.
+     */
+    @Test
+    void generateActuatorId() {
+        assertEquals(1, IdGenerator.generateActuatorId());
+        assertEquals(2, IdGenerator.generateActuatorId());
+        assertEquals(3, IdGenerator.generateActuatorId());
     }
 }
