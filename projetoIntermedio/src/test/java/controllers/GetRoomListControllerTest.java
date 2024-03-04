4f2da6a60de0636598777336d522fa4968c3eec2
franciscanogueira <1231829@isep.ipp.pt>
2024-03-04 15:33:03 +0000

    getRoomListController

--- projetoIntermedio/src/test/java/controllers/GetRoomListControllerTest.java
+++ projetoIntermedio/src/test/java/controllers/GetRoomListControllerTest.java
@@ -1,8 +1,7 @@
 package controllers;
 
-import domain.Dimensions;
+
 import domain.House;
-import domain.Room;
 import dto.RoomDTO;
 import factories.*;
 import factories.implement.*;
@@ -10,82 +9,15 @@ import mappers.RoomMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
+
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
-
+/**
+ * This class is responsible for testing the GetRoomListController class.
+ */
 class GetRoomListControllerTest {
-
-    House house;
-    Room room1;
-    List<Room> rooms;
-    Dimensions dimensions;
-    GetRoomListController getRoomListController;
-    RoomMapper roomMapper;
-
-    /**
-     * This method sets up the testing environment before each test.
-     */
-    @BeforeEach
-    void setUp() throws InstantiationException {
-        roomMapper = new RoomMapper();
-        house = mock(House.class);
-        room1 = mock(Room.class);
-        rooms = new ArrayList<>();
-        rooms.add(room1);
-        when(house.getRooms()).thenReturn(rooms);
-        dimensions = mock(Dimensions.class);
-        when(dimensions.getWidth()).thenReturn(10.9);
-        when(dimensions.getLength()).thenReturn(10.9);
-        when(dimensions.getLength()).thenReturn(10.9);
-        when(room1.getDimensions()).thenReturn(dimensions);
-        getRoomListController = new GetRoomListController(house, roomMapper);
-    }
-
-    /**
-     * Test to check if the constructor of the GetRoomListController class is not null.
-     */
-    @Test
-    void constructor() throws InstantiationException {
-       //Act
-        GetRoomListController result = new GetRoomListController(house, roomMapper);
-       //Assert
-        assertNotNull(result);
-
-    }
-
-    /**
-     * Test to check if the getRoomList method returns an empty list.
-     */
-    @Test
-    void getRoomList_emptyList() throws InstantiationException {
-        //Arrange
-        House house1 = mock(House.class);
-        GetRoomListController getRoomListController = new GetRoomListController(house1, roomMapper);
-        int expected = 0;
-        //Act
-        List<RoomDTO> result = getRoomListController.getRoomList();
-        //Assert
-        assertEquals(expected, result.size());
-    }
-
-    /**
-     * Test to check if the getRoomList method returns a list of rooms.
-     */
-    @Test
-    void getRoomList() {
-        //Arrange
-        int expected = 1;
-        //Act
-        List<RoomDTO> result = getRoomListController.getRoomList();
-        //Assert
-        assertEquals(expected, result.size());
-
-    }
-
-    // --------------------Integration----------------------
 
     GPSFactory gpsFactory;
     LocationFactory locationFactory;
@@ -99,6 +31,9 @@ class GetRoomListControllerTest {
     GetRoomListController getRoomListController1;
     RoomMapper roomMapper1;
 
+    /**
+     * This method sets up the testing environment before each test.
+     */
     @BeforeEach
     void setUpIntegration() throws InstantiationException {
         roomMapper1 = new RoomMapper();
@@ -161,7 +96,7 @@ class GetRoomListControllerTest {
         house2.addRoom("roomName", 0, 10, 10, 10);
         int expected = 1;
         //Act
-        List<RoomDTO> result = getRoomListController.getRoomList();
+        List<RoomDTO> result = getRoomListController1.getRoomList();
         //Assert
         assertEquals(expected, result.size());
 
