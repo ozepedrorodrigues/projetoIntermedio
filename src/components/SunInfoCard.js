a2563441b8ae4ca64af0749400efefabb9dce826
ozepedrorodrigues <1231837@isep.ipp.pt>
2024-06-04 16:08:35 +0100

    .

--- src/components/SunInfoCard.js
+++ src/components/SunInfoCard.js
@@ -1,12 +1,25 @@
-import React from 'react';
 import { FaSun, FaMoon } from 'react-icons/fa';
 import './SunInfoCard.css';
+import React, { useEffect, useState } from 'react';
 
 const SunInfoCard = ({ type }) => {
   const isSunset = type === 'sunset';
   const title = isSunset ? 'Sunset' : 'Sunrise';
-  const time = isSunset ? '18:45' : '06:30'; // Valores fictícios
   const Icon = isSunset ? FaMoon : FaSun;
+
+  const [time, setTime] = useState(isSunset ? '18:45' : '06:30');
+
+  useEffect(() => {
+    fetch('http://10.9.24.170:8080/SunriseSunset')
+      .then(response => response.json())
+      .then(data => {
+        const fetchedTime = isSunset ? data.Sunset : data.Sunrise;
+        setTime(fetchedTime);
+      })
+      .catch(error => {
+        console.error('Error fetching time:', error);
+      });
+  }, [isSunset]);
 
   return (
     <div className="card sun-info-card">
